ef5be70d79a50dc685cd15a9ceba35e72330f8e4
ishandas387 <ishan.das387@gmail.com>
2019-08-02 00:38:01 +0530

    multiple linear regression update

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -46,3 +46,64 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
+
+
+#backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr= np.ones((50,1)).astype(int), values = X,axis=1)
+X_opt = X[:,[0,1,2,3,4,5]]
+#ordinary least sq
+regressor_OLS = sm.OLS(endog=y,exog=X_opt).fit()
+#get each independent variables P value
+#but first get it through summary
+regressor_OLS.summary()
+
+##eleminate the other variables with p>0.05
+X_opt = X[:,[0,1,3,4,5]]
+#ordinary least sq
+regressor_OLS = sm.OLS(endog=y,exog=X_opt).fit()
+#get each independent variables P value
+#but first get it through summary
+regressor_OLS.summary()
+
+
+##eleminate the other variables with p>0.05
+X_opt = X[:,[0,3,4,5]]
+#ordinary least sq
+regressor_OLS = sm.OLS(endog=y,exog=X_opt).fit()
+#get each independent variables P value
+#but first get it through summary
+regressor_OLS.summary()
+
+##eleminate the other variables with p>0.05
+X_opt = X[:,[0,3,5]]
+#ordinary least sq
+regressor_OLS = sm.OLS(endog=y,exog=X_opt).fit()
+#get each independent variables P value
+#but first get it through summary
+regressor_OLS.summary()
+
+X_opt = X[:,[0,3,]]
+#ordinary least sq
+regressor_OLS = sm.OLS(endog=y,exog=X_opt).fit()
+#get each independent variables P value
+#but first get it through summary
+regressor_OLS.summary()
+
+#making the above code automatic
+"""import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)"""
